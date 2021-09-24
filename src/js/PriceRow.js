b296674d68dd17e9d29fd656b1cf5546702e4d0a
Dhruv Pal <paldhruv89@gmail.com>
2021-09-24 12:50:24 +0530

    updated state object in PriceRow

--- src/js/PriceRow.js
+++ src/js/PriceRow.js
@@ -3,24 +3,33 @@ import Container from './VeggieListContainer';
 
 function PriceRow(props) {
   // [total.old, total.new]
-  const [total, setTotal] = useState([0,0]);
+  const [state, setState] = useState({
+    oldTotal: 0,
+    newTotal: 0,
+  });
+
   const price = props.price ?? 10;
   const id = props.id ?? 0;
 
   const handleListUpdate = newCount => {
-    setTotal([total[1], price * newCount]);
+    const curTotal = state.newTotal;
+    setState({
+      oldTotal: curTotal,
+      newTotal: price * newCount,
+    })
   };
+
   useEffect(() => {
     if(props.postUpdate !== undefined) {
-      props.postUpdate(total[0], total[1]);
+      props.postUpdate(state.oldTotal, state.newTotal);
     }
-  }, [total[1]]);
+  }, [state.newTotal]);
 
   return (
     <tr key={id}>
       <td>{price}</td>
       <td><Container rowId={id} postUpdate={handleListUpdate}/></td>
-      <td>{total[1]}</td>
+      <td>{state.newTotal}</td>
     </tr>
   );
 }
